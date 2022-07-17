8c897f383959fd6222567e636fe947848222ef34
uttam kamar <kamaruttam@gmail.com>
2022-07-17 22:19:29 +0530

    manual authetication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -58,4 +58,26 @@ module.exports.create = function (req, res) {
 };
 
 //signin and create a session for the user
-module.exports.createSession = function (req, res) {};
+module.exports.createSession = function (req, res) {
+	//step to authenticate
+	//find the user
+	User.findOne({ email: req.body.email }, function (err, user) {
+		if (err) {
+			console.log('error in finding user in signing In');
+			return;
+		}
+		//handle user found
+		if (user) {
+			//handle password which doesn't match
+			if (user.password != req.body.password) {
+				return res.redirect('back');
+			}
+			//habndle session creation
+			res.cookie('user_id', user.id);
+			return res.redirect('/users/profile');
+		} else {
+			//handle user not found
+			return res.redirect('back');
+		}
+	});
+};
