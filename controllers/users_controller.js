7f3e621f30bdceeb1a70623bd5f0fa3853d64639
uttam kamar <kamaruttam@gmail.com>
2022-07-18 12:25:07 +0530

    passing user data to views and redirect

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -20,6 +20,9 @@ module.exports.post = function (req, res) {
 
 //render the signup page
 module.exports.signUp = function (req, res) {
+	if (req.isAuthenticated()) {
+		return res.redirect('/users/profile');
+	}
 	return res.render('user_sign_up', {
 		title: 'Codeial | SignUp',
 	});
@@ -27,6 +30,9 @@ module.exports.signUp = function (req, res) {
 
 //render the signin page
 module.exports.signIn = function (req, res) {
+	if (req.isAuthenticated()) {
+		return res.redirect('/users/profile');
+	}
 	return res.render('user_sign_in', {
 		title: 'Codeial | SignIn',
 	});
