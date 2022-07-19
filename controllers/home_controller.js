58b47e98ae1e41d376a6cde3c4528200e2484231
uttam kamar <kamaruttam@gmail.com>
2022-07-19 19:03:57 +0530

    display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,26 @@
+const Post = require('../models/post');
+
 module.exports.home = function (req, res) {
 	// return res.end('<h1>Express is up for codeial!</h1>');
-	console.log(req.cookies);
-	res.cookie('user_id', 30);
-	return res.render('home', {
-		title: 'home page',
-		homeBody: 'homePageBody',
-	});
+	// console.log(req.cookies);
+	// res.cookie('user_id', 30);
+
+	// Post.find({}, function (err, posts) {
+	// 	return res.render('home', {
+	// 		title: 'Codeial | Home',
+	// 		homeBody: 'homePageBody',
+	// 		posts: posts,
+	// 	});
+	// });
+
+	//populate the user of each post
+	Post.find({})
+		.populate('user')
+		.exec(function (err, posts) {
+			return res.render('home', {
+				title: 'Codeial | Home',
+				homeBody: 'homePageBody',
+				posts: posts,
+			});
+		});
 };
