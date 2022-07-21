d9690f7d1f5cdff92993f291a7375f3b9511d3c8
uttam kamar <kamaruttam@gmail.com>
2022-07-21 13:27:20 +0530

    display comments and author

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -16,6 +16,12 @@ module.exports.home = function (req, res) {
 	//populate the user of each post
 	Post.find({})
 		.populate('user')
+		.populate({
+			path: 'comments',
+			populate: {
+				path: 'user',
+			},
+		})
 		.exec(function (err, posts) {
 			return res.render('home', {
 				title: 'Codeial | Home',
